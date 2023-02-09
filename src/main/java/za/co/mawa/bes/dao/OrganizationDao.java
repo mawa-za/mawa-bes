88facfb358fcaa5cb4c64aa34f0d08170a7eab45
tshegofatso.tlamama <tshegofatso.tlamama@raretag.co.za>
2023-02-09 11:55:17 +0200

    1. Modified OrganizationDao in the dao directory 2. Modified OrganizationService in the service directory.

--- src/main/java/za/co/mawa/bes/dao/OrganizationDao.java
+++ src/main/java/za/co/mawa/bes/dao/OrganizationDao.java
@@ -1,8 +1,9 @@
 package za.co.mawa.bes.dao;
 
 import za.co.mawa.bes.dto.OrganizationDto;
+import za.co.raretag.mawabes.dto.PartnerQueryDto;
 
 public interface OrganizationDao {
     String addOrganization (OrganizationDto organization);
-    String getOrganization();
+    PartnerQueryDto getOrganization();
 }
