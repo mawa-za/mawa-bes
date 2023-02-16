1ea0ccfbc88fc188ebaeb6c662f5c43e0fb01118
tshegofatso.tlamama <tshegofatso.tlamama@raretag.co.za>
2023-02-16 16:29:59 +0200

    1. Modified PartnerController in the controller directory. 2. Modified PartnerService in the service directory.

--- src/main/java/za/co/mawa/bes/controller/PartnerController.java
+++ src/main/java/za/co/mawa/bes/controller/PartnerController.java
@@ -10,19 +10,26 @@ import org.springframework.web.bind.annotation.*;
 import za.co.mawa.bes.dto.PartnerDto;
 import za.co.mawa.bes.service.PartnerService;
 
+import java.util.ArrayList;
+
 @RestController
 @CrossOrigin
 public class PartnerController {
     Gson gson = new Gson();
     @Autowired
     PartnerService partnerService;
-
-    @RequestMapping(value = "/partners/{id}/role", method = RequestMethod.GET)
-    public ResponseEntity<?> getPartnerRole(@PathVariable String id) {
-        return ResponseEntity.ok(gson.toJson(partnerService.getPartnerRoles(id)));
+//getPartners(String partnerRole)
+    @RequestMapping(value = "/partners/{partnerRole}/role", method = RequestMethod.GET)
+    public ResponseEntity<?> getPartnerRole(@PathVariable String partnerRole) {
+        try{
+            ArrayList<PartnerDto> partnerDto = partnerService.getPartners(partnerRole);
+            return ResponseEntity.ok(gson.toJson(partnerDto));
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
     @RequestMapping(value = "/partners/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> getPartner(@PathVariable String id) {
+    public ResponseEntity<?> getPartnerById(@PathVariable String id) {
         try{
             PartnerDto partnerDto = partnerService.get(id);
             return ResponseEntity.ok(gson.toJson(partnerDto));
@@ -31,7 +38,7 @@ public class PartnerController {
         }
     }
 
-    @RequestMapping(value = "/partners{id}/assign", method = RequestMethod.POST)
+    @RequestMapping(value = "/partners/{id}/assign", method = RequestMethod.POST)
     public ResponseEntity<?> assignPartnerRoleToPartner(@PathVariable String id, @Param("role") String role) {
         try {
         boolean partnerDto = partnerService.addRole(id, role);
