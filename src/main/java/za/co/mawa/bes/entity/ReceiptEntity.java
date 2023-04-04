ee0050e3c607514a8dee717fbbeae1d977c84a14
Tshepang-1911 <tshepang.makhalemele@raretag.co.za>
2023-04-04 10:03:05 +0200

    1.Added a NumberRangeType class to assist with determine the number range. 2.Added new ReceiptController to assist with creating a receipt and return the receipt id. 3.Added ReceiptCreateDto to assist with the object of receipt create. 4.Added ReceiptDao interface class with createReceipt method. 5.Added ReceiptService class to implement createReceipt method which creates the receipt. 6. Added private method entityIDtoDto to return the id of the receipt. 7.Added ReceiptRepository interface which extends JpaRepository<ReceiptEntity, String>. 8.Added ReceiptDto to hold the receipt object. 9.Added ReceiptType class to help with receipt type. 10.Added ReceiptEntity to map values to the DB

---
+++ src/main/java/za/co/mawa/bes/entity/ReceiptEntity.java
@@ -0,0 +1,46 @@
+package za.co.mawa.bes.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "receipt")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class ReceiptEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    @Column(name = "receipt_number" , length = 45)
+    private String receiptNumber;
+    @Column(name = "receipt_type",length = 45)
+    private String receiptType;
+    @Column(name = "creation_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+    @Column(name = "creation_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationTime;
+    @Column(name = "created_by",length = 45)
+    private String createdBy;
+    @Column(name = "invoice_number",length = 45)
+    private String invoiceNumber;
+    @Column(name = "membership_number",length = 45)
+    private String membershipNumber;
+    @Column(name = "membership_period",length = 45)
+    private String membershipPeriod;
+    @Column(name = "tender_type",length = 45)
+    private String tenderType;
+    @Column(name = "amount")
+    private BigDecimal amount;
+}
