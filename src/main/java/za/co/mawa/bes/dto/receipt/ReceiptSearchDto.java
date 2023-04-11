91e07935237d1257459eceae19a83c8dec8dec9b
Tshepang-1911 <tshepang.makhalemele@raretag.co.za>
2023-04-11 08:15:51 +0200

    1.Created Class called ReceiptSearchDto which is used search for receipts. 2.Created a method called getReceipts in ReceiptDao which accepts ReceiptSearchDto object class. 3.Implemented method getReceipts in ReceiptService which will get receipts based on the set value of the ReceiptSearchDto. 4.Created method called findByCriteria in ReceiptService which generates a dynamic SQL filter based on set values from the ReceiptSearchDto. 5.Created method called entityArrayToDto which converts List<ReceiptEntity> to ArrayList<ReceiptDto>. 6.Created method called entityToDto which converts ReceiptEntity to ReceiptDto. 7.Created a method called findAll in the ReceiptRepository which accepts Specification<ReceiptEntity> and Sort parameters. 8.Created method called getReceipts in ReceiptController which accept the requested query param and sets the values to the ReceiptSearchDto and returns the receipts as a json based on filters.

---
+++ src/main/java/za/co/mawa/bes/dto/receipt/ReceiptSearchDto.java
@@ -0,0 +1,21 @@
+package za.co.mawa.bes.dto.receipt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ReceiptSearchDto implements Serializable{
+    private String receiptType;
+    private String invoiceNumber;
+    private String membershipNumber;
+    private String membershipPeriod;
+    private String tenderType;
+    private String createdBy;
+}
