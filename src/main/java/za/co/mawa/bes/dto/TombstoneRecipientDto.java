33be6a427e9b478d4257c0aa239f26950cc704e3
Tshepang-1911 <tshepang.makhalemele@raretag.co.za>
2023-09-27 10:36:25 +0200

    Tombstone Recipient Endpoint #337

---
+++ src/main/java/za/co/mawa/bes/dto/TombstoneRecipientDto.java
@@ -0,0 +1,20 @@
+package za.co.mawa.bes.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@NoArgsConstructor
+@Getter
+@Setter
+public class TombstoneRecipientDto implements Serializable {
+    private String id;
+    private String title;
+    private String idType;
+    private String idNumber;
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private String gender;
+}
