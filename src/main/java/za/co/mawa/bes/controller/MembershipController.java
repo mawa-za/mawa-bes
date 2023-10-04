08c72177a5a73b49292051017a6776fa152c7ed4
Tebogo Mohale <tebogo.mohale@raretag.co.za>
2023-10-04 17:53:45 +0200

    Membership Create Error #373

--- src/main/java/za/co/mawa/bes/controller/MembershipController.java
+++ src/main/java/za/co/mawa/bes/controller/MembershipController.java
@@ -50,6 +50,8 @@ public class MembershipController {
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> postMembership(@RequestBody MembershipCreateDto membershipCreateDto) {
         try {
+            System.out.println("Calling membershipService.create");
+            System.out.println(membershipCreateDto.toString());
             return ResponseEntity.ok(gson.toJson(membershipService.create(membershipCreateDto)));
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
