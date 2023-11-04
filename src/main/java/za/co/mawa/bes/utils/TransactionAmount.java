98203479e48bda9cefef4033c9cc37578a21f013
tebogo.mohale <tebogo.mohale@raretag.co.za>
2023-11-04 13:30:43 +0200

    Return monthly premium on membership

---
+++ src/main/java/za/co/mawa/bes/utils/TransactionAmount.java
@@ -0,0 +1,17 @@
+package za.co.mawa.bes.utils;
+
+public class TransactionAmount {
+    public static String TOTAL_EXC_VAT = "TOTAL-EXC-VAT";
+    public static String TOTAL_INC_VAT = "TOTAL-INC-VAT";
+    public static String DISCOUNT_AMOUNT = "DISCOUNT-AMOUNT";
+    public static String DISCOUNT_PERCENT = "DISCOUNT-PERCENT";
+    public static String VAT_AMOUNT = "VAT-AMOUNT";
+    public static String VAT_PERCENT = "VAT-PERCENT";
+    public static String SELLING_PRICE = "SELLING-PRICE";
+    public static String AMOUNT_COLLECTED = "AMOUNT-COLLECTED";
+    public static String AMOUNT_DEPOSITED = "AMOUNT-DEPOSITED";
+    public static String PAYMENT_AMOUNT = "PAYMENT-AMOUNT";
+    public static String DEPOSIT_AMOUNT = "DEPOSIT-AMOUNT";
+    public static String VOUCHER_AMOUNT = "VOUCHER-AMOUNT";
+    public static String MONTHLY_PREMIUM = "MONTHLY-PREMIUM";
+}
