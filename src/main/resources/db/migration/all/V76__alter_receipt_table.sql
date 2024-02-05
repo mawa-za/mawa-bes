e4458a592af3c910ddc5d09d87daa6aea81e6d91
tebogo.mohale <tebogo.mohale@raretag.co.za>
2024-02-05 13:28:29 +0200

    Add transaction id column on receipt table

--- src/main/resources/db/migration/all/V76__alter_receipt_table.sql
+++ src/main/resources/db/migration/all/V76__alter_receipt_table.sql
@@ -1,5 +1,3 @@
-  INSERT INTO `field_option` (`code`, `field`, `description`, `valid_from`, `valid_to`)
-  VALUES ('TOTAL-WITHDRAWN', 'TRANSACTION-AMOUNT', 'Total Withdrawn', CURRENT_DATE, '9999-12-31');
+ALTER TABLE `receipt`
+ADD COLUMN `transaction_id` VARCHAR(45) NULL AFTER `receipt_type`;
 
-  INSERT INTO `field_option` (`code`, `field`, `description`, `valid_from`, `valid_to`)
-  VALUES ('TOTAL-DEPOSITED', 'TRANSACTION-AMOUNT', 'Total Deposited', CURRENT_DATE, '9999-12-31');
