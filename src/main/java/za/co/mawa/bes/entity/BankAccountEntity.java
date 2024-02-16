03bf25d992c0f401d4644dcdc033ebbea73d2b4a
tebogo.mohale <tebogo.mohale@raretag.co.za>
2024-02-16 22:16:25 +0200

    Add new Banking Details table

---
+++ src/main/java/za/co/mawa/bes/entity/BankAccountEntity.java
@@ -0,0 +1,46 @@
+package za.co.mawa.bes.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "bank_account")
+public class BankAccountEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    @Column(name = "object_id")
+    private String objectId;
+    @Column(name = "account_holder")
+    private String accountHolder;
+    @Column(name = "bank_name")
+    private String bankName;
+    @Column(name = "account_number")
+    private String accountNumber;
+    @Column(name = "account_type")
+    private String accountType;
+    @Column(name = "branch_code")
+    private String branchCode;
+    @Column(name = "branch_name")
+    private String branchName;
+    @Column(name = "valid_from")
+    @Temporal(TemporalType.DATE)
+    private Date validFrom;
+    @Column(name = "valid_to")
+    @Temporal(TemporalType.DATE)
+    private Date validTo;
+    @Column(name = "status")
+    private String status;
+}
