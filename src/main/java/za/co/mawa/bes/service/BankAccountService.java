7c6a0038e75efb199b66070fd17e22b3999789a9
tebogo.mohale <tebogo.mohale@raretag.co.za>
2024-02-19 07:53:14 +0200

    New Bank Account Endpoints

--- src/main/java/za/co/mawa/bes/service/BankAccountService.java
+++ src/main/java/za/co/mawa/bes/service/BankAccountService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.co.mawa.bes.dto.BankAccountCreateDto;
 import za.co.mawa.bes.dto.BankAccountDto;
+import za.co.mawa.bes.dto.BankAccountEditDto;
 import za.co.mawa.bes.dto.transaction.bank.account.TransactionBankAccountDto;
 import za.co.mawa.bes.entity.BankAccountEntity;
 import za.co.mawa.bes.entity.transaction.TransactionBankAccount;
@@ -13,7 +14,9 @@ import za.co.mawa.bes.utils.Constant;
 import za.co.mawa.bes.utils.Field;
 import za.co.mawa.bes.utils.Status;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class BankAccountService {
@@ -21,7 +24,8 @@ public class BankAccountService {
     BankAccountRepository bankAccountRepository;
     @Autowired
     FieldOptionService fieldOptionService;
-    public void save(BankAccountCreateDto bankAccountCreateDto){
+
+    public void add(BankAccountCreateDto bankAccountCreateDto) {
         BankAccountEntity bankAccountEntity = new BankAccountEntity();
         bankAccountEntity.setObjectId(bankAccountCreateDto.getObjectId());
         bankAccountEntity.setAccountHolder(bankAccountCreateDto.getAccountHolder());
@@ -34,14 +38,37 @@ public class BankAccountService {
         bankAccountEntity.setValidTo(new Date(Constant.END_DATE));
         bankAccountRepository.save(bankAccountEntity);
     }
+    public void edit(BankAccountEditDto bankAccountEditDto) {
+        BankAccountEntity bankAccountEntity = bankAccountRepository.getById(bankAccountEditDto.getId());
+        bankAccountEntity.setAccountHolder(bankAccountEditDto.getAccountHolder());
+        bankAccountEntity.setAccountType(bankAccountEditDto.getAccountType());
+        bankAccountEntity.setBankName(bankAccountEditDto.getBankName());
+        bankAccountEntity.setAccountNumber(bankAccountEditDto.getAccountNumber());
+        bankAccountEntity.setBranchCode(bankAccountEditDto.getBranchCode());
+//        bankAccountEntity.setStatus(Status.ACTIVE);
+        bankAccountEntity.setValidFrom(new Date());
+        bankAccountEntity.setValidTo(new Date(Constant.END_DATE));
+        bankAccountRepository.save(bankAccountEntity);
+    }
+    public List<BankAccountDto> getList(String objectId) {
+        List<BankAccountDto> bankAccountDtoList = new ArrayList<>();
+        List<BankAccountEntity> bankAccountEntityList = bankAccountRepository.getByObjectId(objectId);
+        for (BankAccountEntity bankAccountEntity : bankAccountEntityList) {
+            bankAccountDtoList.add(get(bankAccountEntity.getId()));
+        }
+        return bankAccountDtoList;
+    }
+
     public BankAccountDto get(String id) {
-        BankAccountEntity bankAccountEntity = bankAccountRepository.getByObjectId(id);
+        BankAccountEntity bankAccountEntity = bankAccountRepository.getById(id);
         BankAccountDto bankAccountDto = new BankAccountDto();
+        bankAccountDto.setId(bankAccountEntity.getId());
         bankAccountDto.setAccountHolder(bankAccountEntity.getAccountHolder());
         bankAccountDto.setAccountNumber(bankAccountEntity.getAccountNumber());
         bankAccountDto.setBankName(fieldOptionService.getFieldOption(Field.BANK_NAME, bankAccountEntity.getBankName()));
         bankAccountDto.setAccountType(fieldOptionService.getFieldOption(Field.BANK_ACCOUNT_TYPE, bankAccountEntity.getAccountType()));
         bankAccountDto.setBranchCode(bankAccountEntity.getBranchCode());
+        bankAccountDto.setStatus(fieldOptionService.getFieldOption(Field.STATUS, bankAccountEntity.getStatus()));
         return bankAccountDto;
     }
 
