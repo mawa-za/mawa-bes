4389173fcdd2a42b30b7814392e30e899bcf3459
tebogo.mohale <tebogo.mohale@raretag.co.za>
2024-02-19 18:36:53 +0200

    Receipts Get Query Parameters adjustment

--- src/main/java/za/co/mawa/bes/controller/ReceiptController.java
+++ src/main/java/za/co/mawa/bes/controller/ReceiptController.java
@@ -67,8 +67,7 @@ public class ReceiptController {
     @RequestMapping(value = "/receipts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getReceipts(@RequestParam(required = false) String receiptType,
                                          @RequestParam(required = false) String invoiceNumber,
-                                         @RequestParam(required = false) String membershipNumber,
-                                         @RequestParam(required = false) String membershipPeriod,
+                                         @RequestParam(required = false) String transaction,
                                          @RequestParam(required = false) String tenderType,
                                          @RequestParam(required = false) boolean notCashed,
                                          @RequestParam(name = "user", required = false) String createdBy) {
@@ -80,11 +79,8 @@ public class ReceiptController {
             if (invoiceNumber != null && invoiceNumber != "") {
                 search.setInvoiceNumber(invoiceNumber);
             }
-            if (membershipNumber != null && membershipNumber != "") {
-                search.setMembershipNumber(membershipNumber);
-            }
-            if (membershipPeriod != null && membershipPeriod != "") {
-                search.setMembershipPeriod(membershipPeriod);
+            if (transaction != null && transaction != "") {
+                search.setTransaction(transaction);
             }
             if (tenderType != null && tenderType != "") {
                 search.setTenderType(tenderType);
