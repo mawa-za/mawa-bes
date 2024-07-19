33e331bced17b12fd5f25534ab517af1316ae990
funi504 <nekhungunifunanani9@gmail.com>
2024-07-19 13:12:31 +0200

    claim  reject 401 - updated

--- src/main/java/za/co/mawa/bes/service/ClaimService.java
+++ src/main/java/za/co/mawa/bes/service/ClaimService.java
@@ -185,7 +185,7 @@ public class ClaimService {
             claimOutboundDto.setId(transactionDto.getId());
             claimOutboundDto.setNumber(transactionDto.getNumber());
             claimOutboundDto.setStatus(fieldOptionService.getFieldOption(Field.TRANSACTION_STATUS, transactionDto.getStatus()));
-            claimOutboundDto.setStatusReason(fieldOptionService.getFieldOption(Field.STATUS_REASON, transactionDto.getStatusReason().toUpperCase()));
+            claimOutboundDto.setStatusReason(fieldOptionService.getFieldOption(Field.CLAIM_DECLINE_REASON, transactionDto.getStatusReason().toUpperCase()));
             //claimOutboundDto.setStatusReason(fieldOptionService.getFieldOption(Field.PAYMENT_REQUEST_STATUS_REASON, transactionDto.getStatusReason().toUpperCase()));
 
             if(transactionDto.getDescription() == null ){
