48db41b0ab41adf5b9ba95266dc5121f0dcb4d6e
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2024-07-26 08:37:37 +0000

    ""

--- src/main/java/za/co/mawa/bes/controller/UserController.java
+++ src/main/java/za/co/mawa/bes/controller/UserController.java
@@ -50,51 +50,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/username/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getUserByName(@PathVariable String username) {
-        try {
-            UserDto userDto = userService.getUserByName(username);
-            userDto.setPassword(null);
-            return ResponseEntity.ok(gson.toJson(userDto));
-        } catch (Exception exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception);
-        }
-    }
-
-    @GetMapping(value = "/email/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getUserByEmail(@PathVariable String email) {
-        try {
-            UserDto userDto = userService.getUserByEmail(email);
-            userDto.setPassword(null);
-            return ResponseEntity.ok(gson.toJson(userDto));
-        } catch (Exception exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
-        }
-    }
-
-    @GetMapping(value = "/cellphone/{cellphone}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getUserByCellphone(@PathVariable String cellphone) {
-        try {
-            UserDto userDto = userService.getUserByCellphone(cellphone);
-            userDto.setPassword(null);
-            return ResponseEntity.ok(gson.toJson(userDto));
-        } catch (Exception exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
-        }
-    }
-
-    @GetMapping(value = "/partner/{partnerId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getUserByPartnerId(@PathVariable String partnerId) {
-        try {
-            UserDto userDto = userService.getUserByPartnerId(partnerId);
-            userDto.setPassword(null);
-            return ResponseEntity.ok(gson.toJson(userDto));
-        } catch (Exception exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
-        }
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getUser(@PathVariable String id) {
         try {
             UserDto userDto = userService.getUserById(id);
@@ -105,12 +61,12 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/{id}/role", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}/role", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getRoles(@PathVariable String id) throws Exception {
         return ResponseEntity.ok(userService.getRoles(id));
     }
 
-    @RequestMapping(value = "/{id}/role", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}/role", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> addRole(@PathVariable String id, @RequestBody List<String> roleList) throws Exception {
         try {
             for (String role : roleList) {
@@ -125,7 +81,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/{id}/lock", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}/lock", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> lockUser(@PathVariable String id, @RequestParam("reason") String reason) {
         try {
             return ResponseEntity.ok(gson.toJson(userService.lockuser(id, reason)));
@@ -134,7 +90,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/{id}/unlock", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}/unlock", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> unlockUser(@PathVariable String id) {
         try {
             return ResponseEntity.ok(gson.toJson(userService.unlockuser(id)));
@@ -143,7 +99,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/{id}/role", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}/role", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> deleteRole(@PathVariable String id, @RequestParam("userRole") String userRole) throws Exception {
         try {
             UserRolePKEntity pkEntity = new UserRolePKEntity();
@@ -164,7 +120,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/{id}/reset", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}/reset", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> restUser(@PathVariable String id) throws Exception {
         try {
             return ResponseEntity.ok().body(gson.toJson(userService.resetUser(id)));
@@ -173,12 +129,56 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/{id}/edit", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> editUser(@PathVariable String id, @RequestBody UserEditDto editDto) throws Exception {
         try {
             return ResponseEntity.ok().body(gson.toJson(userService.editUser(id, editDto)));
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception);
+        }
+    }
+
+    @RequestMapping(value = "/username/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserByUsername(@PathVariable String username) {
+        try {
+            UserDto userDto = userService.getUserByName(username);
+            userDto.setPassword(null);
+            return ResponseEntity.ok(gson.toJson(userDto));
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception);
+        }
+    }
+
+    @RequestMapping(value = "/email/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserByEmail(@PathVariable String email) {
+        try {
+            UserDto userDto = userService.getUserByEmail(email);
+            userDto.setPassword(null);
+            return ResponseEntity.ok(gson.toJson(userDto));
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/cellphone/{cellphone}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserByCellphone(@PathVariable String cellphone) {
+        try {
+            UserDto userDto = userService.getUserByCellphone(cellphone);
+            userDto.setPassword(null);
+            return ResponseEntity.ok(gson.toJson(userDto));
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/partner/{partnerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserByPartnerId(@PathVariable String partnerId) {
+        try {
+            UserDto userDto = userService.getUserByPartnerId(partnerId);
+            userDto.setPassword(null);
+            return ResponseEntity.ok(gson.toJson(userDto));
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         }
     }
 }
