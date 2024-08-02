10c0ed04ba71d8081970434c322a70dc9e2683de
tebogo.mohale <tebogo.mohale@raretag.co.za>
2024-08-02 22:48:48 +0200

    Add Partner Roles

---
+++ src/main/resources/db/migration/all/V109__add_partner_roles.sql
@@ -0,0 +1,5 @@
+INSERT INTO `field_option` (`code`, `field`, `description`, `valid_from`, `valid_to`)
+  VALUES ('CUSTOMER', 'PARTNER-ROLE', 'Customer', CURRENT_DATE, '9999-12-31');
+
+  INSERT INTO `field_option` (`code`, `field`, `description`, `valid_from`, `valid_to`)
+    VALUES ('EMPLOYEE', 'PARTNER-ROLE', 'Employee', CURRENT_DATE, '9999-12-31');
