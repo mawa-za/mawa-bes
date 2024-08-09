5352c82f7c8f13407e9d5a3f7c348c3517a66553
tebogo.mohale <tebogo.mohale@raretag.co.za>
2024-08-09 17:18:28 +0200

    Generate object numbers in the DB

---
+++ src/main/resources/db/migration/all/V110__stored_procedure_to_generate_numbers.sql
@@ -0,0 +1,25 @@
+DELIMITER $$
+DROP procedure IF EXISTS `GetNewNumber`$$
+DROP function IF EXISTS `generateNumber`$$
+CREATE FUNCTION generateNumber(objectIn VARCHAR(50))
+   RETURNS VARCHAR(20)
+   DETERMINISTIC
+   BEGIN
+	DECLARE finalNumber VARCHAR(20);
+	DECLARE newNumber int;
+	DECLARE currentNumber VARCHAR(20);
+    DECLARE currentPrefix varchar(20);
+	SELECT current, prefix into currentNumber, currentPrefix FROM number_range where object = objectIn LIMIT 1;
+	set currentPrefix = IFNULL(currentPrefix, '');
+	SET newNumber = REPLACE(currentNumber, currentPrefix, '') + 1;
+    SET finalNumber = CONCAT(currentPrefix, lpad(newNumber,10,0 ));
+    UPDATE number_range SET current = finalNumber where object = objectIn;
+	return finalNumber;
+   END$$
+
+CREATE PROCEDURE GetNewNumber (
+	IN  objectIn VARCHAR(25)
+)
+BEGIN
+	SELECT generateNumber(objectIn);
+END$$
