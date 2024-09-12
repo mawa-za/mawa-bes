f5bdffc577841aad51feb9559983c48f3facae2b
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2024-09-12 17:15:33 +0200

    Revert "1076-invoice-edit-endpoint-needed"

--- src/main/java/za/co/mawa/bes/service/LineItemService.java
+++ src/main/java/za/co/mawa/bes/service/LineItemService.java
@@ -3,6 +3,8 @@ package za.co.mawa.bes.service;
 import com.nimbusds.jose.shaded.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import za.co.mawa.bes.dto.LineItemDto;
+import za.co.mawa.bes.dto.LineItemEditDto;
 import za.co.mawa.bes.dto.LineItemInboundDto;
 import za.co.mawa.bes.dto.LineItemOutboundDto;
 import za.co.mawa.bes.dto.product.ProductDto;
@@ -14,10 +16,8 @@ import za.co.mawa.bes.utils.Field;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
-
 @Service
 public class LineItemService {
     @Autowired
@@ -35,76 +35,49 @@ public class LineItemService {
         try{
             List<LineItemOutboundDto> lineItemOutboundDtoList = new ArrayList<>();
             List<TransactionItemDto> transactionItemDtoList = transactionService.getItems(transaction);
+            BigDecimal vatPercentage = new BigDecimal("0.15");
 
             for(TransactionItemDto transactionItemDto: transactionItemDtoList){
                 LineItemOutboundDto lineItemOutboundDto = new LineItemOutboundDto();
                 lineItemOutboundDto.setTransaction(transactionItemDto.getTransaction());
                 lineItemOutboundDto.setItem(transactionItemDto.getItem());
-
-                try {
-                    lineItemOutboundDto.setProduct(productService.get(transactionItemDto.getProduct()));
-                } catch (Exception e) {
-                }
-
-                BigDecimal unitPrice = transactionItemDto.getUnitPrice().setScale(2, RoundingMode.HALF_UP);
-                lineItemOutboundDto.setUnitPrice(unitPrice);
-
-                BigDecimal quantity = (transactionItemDto.getQuantity().compareTo(BigDecimal.ZERO) == 0)
-                        ? BigDecimal.valueOf(1)
-                        : transactionItemDto.getQuantity().setScale(2, RoundingMode.HALF_UP);
-                lineItemOutboundDto.setQuantity(quantity);
-
+                lineItemOutboundDto.setProduct(productService.get(transactionItemDto.getProduct()));
+                lineItemOutboundDto.setUnitPrice(transactionItemDto.getUnitPrice());
+                lineItemOutboundDto.setQuantity(transactionItemDto.getQuantity());
                 lineItemOutboundDto.setUom(fieldOptionService.getFieldOption(Field.UOM, transactionItemDto.getBaseUnitOfMeasure()));
+//                if(lineItemOutboundDto.getUnitPrice() != null){
+//                    lineItemOutboundDto.setLineTotal(lineItemOutboundDto.getQuantity().multiply(lineItemOutboundDto.getUnitPrice()));
+//                    lineItemOutboundDto.setTotalIncVat(lineItemOutboundDto.getUnitPrice().multiply(BigDecimal.valueOf(0.15).plus()));
+//                    lineItemOutboundDto.setVATAmount((lineItemOutboundDto.getUnitPrice().multiply(BigDecimal.valueOf(0.15))));
+//                }
+                if (lineItemOutboundDto.getUnitPrice() != null && lineItemOutboundDto.getQuantity() != null) {
+                    BigDecimal totalExcVat = lineItemOutboundDto.getUnitPrice().multiply(lineItemOutboundDto.getQuantity());
+                    lineItemOutboundDto.setTotalExcVat(totalExcVat);
 
-                BigDecimal lineTotal = quantity.multiply(unitPrice).setScale(2, RoundingMode.HALF_UP);
-                lineItemOutboundDto.setLineTotal(lineTotal);
+                    lineItemOutboundDto.setLineTotal(lineItemOutboundDto.getQuantity().multiply(lineItemOutboundDto.getUnitPrice()));
 
-                BigDecimal discountPercentage = lineItemOutboundDto.getDiscountPercentage();
-                BigDecimal discountAmount;
+                    BigDecimal vatAmount = totalExcVat.multiply(vatPercentage);
+                    lineItemOutboundDto.setVATAmount(vatAmount);
 
-                boolean isVatInclusive = lineItemOutboundDto.isVatInclusive();
+                    BigDecimal totalIncVat = totalExcVat.add(vatAmount);
+                    lineItemOutboundDto.setTotalIncVat(totalIncVat);
 
-                if (discountPercentage != null && discountPercentage.compareTo(BigDecimal.ZERO) != 0 && !isValidToDateReached(transactionItemDto.getValidTo())) {
-                    discountAmount = lineTotal.multiply(discountPercentage).divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP);
+                    BigDecimal discountAmount = new BigDecimal("0");
                     lineItemOutboundDto.setDiscountAmount(discountAmount);
 
-                    BigDecimal totalExcAfterDiscount = lineTotal.subtract(discountAmount).setScale(2, RoundingMode.HALF_UP);
-                    lineItemOutboundDto.setTotExcVat(totalExcAfterDiscount);
+                    BigDecimal discountPercentage = new BigDecimal("0");
 
-                    if (isVatInclusive) {
-                        BigDecimal vatAmountAfterDiscount = totalExcAfterDiscount.multiply(vatPercentage).divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP);
-                        BigDecimal totalIncAfterDiscount = totalExcAfterDiscount.add(vatAmountAfterDiscount).setScale(2, RoundingMode.HALF_UP);
-
-                        lineItemOutboundDto.setVatAmount(vatAmountAfterDiscount);
-                        lineItemOutboundDto.setTotIncVat(totalIncAfterDiscount);
-                    } else {
-                        lineItemOutboundDto.setVatAmount(BigDecimal.ZERO);
-                        lineItemOutboundDto.setTotIncVat(totalExcAfterDiscount);
+                    if (totalExcVat.compareTo(BigDecimal.ZERO) != 0) {
+                        discountPercentage = discountAmount.divide(totalExcVat, 2, RoundingMode.HALF_UP).multiply(new BigDecimal("100"));
                     }
+                    lineItemOutboundDto.setDiscountPercentage(discountPercentage);
 
-                } else {
-                    lineItemOutboundDto.setTotExcVat(lineTotal.setScale(2, RoundingMode.HALF_UP));
-
-                    if (isVatInclusive) {
-                        BigDecimal vatAmount = lineTotal.multiply(vatPercentage).divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP);
-                        BigDecimal totalIncVat = lineTotal.add(vatAmount).setScale(2, RoundingMode.HALF_UP);
-
-                        lineItemOutboundDto.setVatAmount(vatAmount);
-                        lineItemOutboundDto.setTotIncVat(totalIncVat);
-                    } else {
-                        lineItemOutboundDto.setVatAmount(BigDecimal.ZERO);
-                        lineItemOutboundDto.setTotIncVat(lineTotal);
-                    }
+                    lineItemOutboundDto.setVATPercentage(vatPercentage.multiply(new BigDecimal("100")));
                 }
-
-                lineItemOutboundDto.setVatPercentage(isVatInclusive ? vatPercentage.setScale(2, RoundingMode.HALF_UP) : BigDecimal.ZERO);
-
                 lineItemOutboundDtoList.add(lineItemOutboundDto);
             }
             return lineItemOutboundDtoList;
-
-        } catch (Exception ex) {
-
+        }catch (Exception ex){
             throw new RuntimeException(ex);
         }
 
@@ -124,7 +97,6 @@ public class LineItemService {
         }
 
     }
-
     public void edit(LineItemInboundDto lineItemInboundDto) {
         try{
             TransactionItemPKEntity pkEntity = new TransactionItemPKEntity();
@@ -142,39 +114,31 @@ public class LineItemService {
             }
             transactionItemRepository.save(entity);
         }catch(Exception ex){
-            throw new RuntimeException(ex);
+          throw new RuntimeException(ex);
         }
     }
-
     public void delete(String id) {
 
     }
-
     public void deleteItem(String id,String itemId) throws Exception{
         try{
-            if(itemId != ""){
-                TransactionItemPKEntity pkEntity = new TransactionItemPKEntity();
-                pkEntity.setItem(itemId);
-                pkEntity.setTransaction(id);
-                transactionItemRepository.deleteById(pkEntity);
-            }
-            else{
-                for(TransactionItemDto item: transactionService.getItems(id)){
-                    TransactionItemPKEntity pkEntity = new TransactionItemPKEntity();
-                    pkEntity.setItem(item.getItem());
-                    pkEntity.setTransaction(id);
-                    transactionItemRepository.deleteById(pkEntity);
-                }
-            }
+           if(itemId != ""){
+               TransactionItemPKEntity pkEntity = new TransactionItemPKEntity();
+               pkEntity.setItem(itemId);
+               pkEntity.setTransaction(id);
+               transactionItemRepository.deleteById(pkEntity);
+           }
+           else{
+               for(TransactionItemDto item: transactionService.getItems(id)){
+                   TransactionItemPKEntity pkEntity = new TransactionItemPKEntity();
+                   pkEntity.setItem(item.getItem());
+                   pkEntity.setTransaction(id);
+                   transactionItemRepository.deleteById(pkEntity);
+               }
+           }
         }catch (Exception ex){
-            throw new RuntimeException(ex);
+           throw new RuntimeException(ex);
         }
 
-    }
-
-    public static boolean isValidToDateReached(Date validTo) {
-        Date currentDate = new Date(System.currentTimeMillis());
-
-        return !currentDate.before(validTo);
     }
 }
