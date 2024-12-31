bc031ea4aefc34748a0696779ba24dd579aff056
tebogo.mohale <tebogo.mohale@raretag.co.za>
2024-12-31 09:10:49 +0200

    MSBS-572 - Backend: Create the batch claim payment request endpoint

--- src/main/java/za/co/mawa/bes/controller/BatchController.java
+++ src/main/java/za/co/mawa/bes/controller/BatchController.java
@@ -54,7 +54,7 @@ public class BatchController {
                 EmailDto emailDto = new EmailDto();
                 emailDto.getFiles().add(file);
                 emailDto.setTo(getEmail());
-                emailDto.setSubject("Payment Batch: "+ file.getName());
+                emailDto.setSubject(file.getOwner()+ " Payment Batch: "+ file.getName());
                 emailDto.setTemplate("payment-file-generated");
                 List<PropertyDto> props = new ArrayList<>();
                 props.add(new PropertyDto(HtmlTemplateVariableKey.IDENTIFIER, file.getName()));
