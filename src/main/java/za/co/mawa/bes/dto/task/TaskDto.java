d1a98ba1f91e7fabb35fca7d0b392c936729b909
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2025-01-31 11:10:39 +0200

    Task Additional Fields

--- src/main/java/za/co/mawa/bes/dto/task/TaskDto.java
+++ src/main/java/za/co/mawa/bes/dto/task/TaskDto.java
@@ -3,7 +3,7 @@ package za.co.mawa.bes.dto.task;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import za.co.mawa.bes.dto.partner.PartnerDto;
 import java.io.Serializable;
 @NoArgsConstructor
 @Getter
@@ -15,13 +15,15 @@ public class TaskDto implements Serializable {
     private String type;
     private String description;
     private String customerId;
-    private String customer;
-    private String employeeResponsibleId;
-    private String employeeResponsible;
+    private PartnerDto customer;
+    private PartnerDto employeeResponsibleId;
+    private PartnerDto employeeResponsible;
     private String plannedStartDate;
     private String plannedEndDate;
     private String actualStartDate;
     private String actualEndDate;
     private String status;
+    private String startDate;
+    private String duration;
 }
 
--- src/main/java/za/co/mawa/bes/service/TaskService.java
+++ src/main/java/za/co/mawa/bes/service/TaskService.java
@@ -6,6 +6,10 @@ import za.co.mawa.bes.configuration.context.UserContext;
 import za.co.mawa.bes.dao.TaskDao;
 import za.co.mawa.bes.dto.task.*;
 import za.co.mawa.bes.dto.transaction.*;
+import za.co.mawa.bes.dto.transaction.attribute.TransactionAttributeDto;
+import za.co.mawa.bes.dto.transaction.partner.TransactionPartnerDto;
+import za.co.mawa.bes.entity.transaction.TransactionAttributeEntity;
+import za.co.mawa.bes.exception.PartnerNotFoundException;
 import za.co.mawa.bes.exception.TransactionNotFound;
 import za.co.mawa.bes.utils.Conversion;
 import za.co.mawa.bes.utils.DateType;
@@ -14,11 +18,21 @@ import za.co.mawa.bes.utils.TransactionType;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import za.co.mawa.bes.utils.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Objects;
 
 @Component
 public class TaskService implements TaskDao {
     @Autowired
     TransactionService transactionService;
+    @Autowired
+    TransactionAttributeService transactionAttributeService;
+    @Autowired
+    PartnerService partnerService;
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
 
     @Override
     public TaskDto create(TaskCreateDto taskCreateDto) {
@@ -55,6 +69,37 @@ public class TaskService implements TaskDao {
                 transactionDateDto.setValue(Conversion.stringToDate(taskCreateDto.getPlannedEndDate()));
                 transactionService.addDate(transactionDateDto);
             }
+
+            if(taskCreateDto.getStartDate() != null){
+                TransactionDateDto transactionDateDto = new TransactionDateDto();
+                transactionDateDto.setTransaction(taskId);
+                transactionDateDto.setType(DateType.START_DATE);
+                transactionDateDto.setValue(Conversion.stringToDate(taskCreateDto.getStartDate()));
+                transactionService.addDate(transactionDateDto);
+            }
+            if(taskCreateDto.getDuration() != null){
+                TransactionAttributeDto attributeDto = new TransactionAttributeDto();
+                attributeDto.setAttribute("DURATION");
+                attributeDto.setValue(taskCreateDto.getDuration());
+                attributeDto.setTransaction(taskId);
+                transactionAttributeService.add(attributeDto);
+            }
+
+            if (taskCreateDto.getEmployeeResponsibleId() != null) {
+                TransactionPartnerDto transactionPartnerDto = new TransactionPartnerDto();
+                transactionPartnerDto.setTransaction(taskId);
+                transactionPartnerDto.setFunction(PartnerFunction.EMPLOYEE_RESPONSIBLE);
+                transactionPartnerDto.setPartner(taskCreateDto.getEmployeeResponsibleId());
+                transactionService.addPartner(transactionPartnerDto);
+            }
+
+            if (taskCreateDto.getEmployeeResponsibleId() != null) {
+                TransactionPartnerDto transactionPartnerDto = new TransactionPartnerDto();
+                transactionPartnerDto.setTransaction(taskId);
+                transactionPartnerDto.setFunction(PartnerFunction.CUSTOMER);
+                transactionPartnerDto.setPartner(taskCreateDto.getCustomerId());
+                transactionService.addPartner(transactionPartnerDto);
+            }
             return taskDto;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -71,6 +116,45 @@ public class TaskService implements TaskDao {
             taskDto.setDescription(transactionDto.getDescription());
             taskDto.setNumber(transactionDto.getNumber());
             taskDto.setType(transactionDto.getSubType());
+
+            for (TransactionDateDto transactionDateDto : transactionService.getDates(id)) {
+                String formattedDate = Conversion.dateToString(transactionDateDto.getValue());
+
+                if( transactionDateDto.getType().equals(DateType.PLANNED_START_DATE)){
+                    taskDto.setPlannedStartDate(formattedDate);
+                }
+                if( transactionDateDto.getType().equals(DateType.PLANNED_END_DATE)){
+                    taskDto.setPlannedEndDate(formattedDate);
+                }
+                if(transactionDateDto.getType().equalsIgnoreCase(DateType.START_DATE)){
+                    taskDto.setStartDate(formattedDate);
+                }
+            }
+            try{
+                for (TransactionPartnerDto transactionPartnerDto : transactionService.getPartners(id)) {
+                    if (Objects.equals(transactionPartnerDto.getFunction(), PartnerFunction.CUSTOMER)) {
+                        if (partnerService.get(transactionPartnerDto.getPartner()) != null) {
+                            taskDto.setCustomer(partnerService.get(transactionPartnerDto.getPartner()));
+                        }
+                    }
+                    if (Objects.equals(transactionPartnerDto.getFunction(), PartnerFunction.EMPLOYEE_RESPONSIBLE)) {
+                        if (partnerService.get(transactionPartnerDto.getPartner()) != null) {
+                            taskDto.setEmployeeResponsible((partnerService.get(transactionPartnerDto.getPartner())));
+                        }
+                    }
+                }
+            }catch (Exception e){
+
+            }
+
+
+            List<TransactionAttributeEntity> entities = transactionAttributeService.getByTransactionId(id);
+            for(TransactionAttributeEntity entity : entities){
+                if(entity.getAttribute().equalsIgnoreCase("DURATION")){
+                    taskDto.setDuration(entity.getValue());
+                }
+            }
+
             return taskDto;
         } catch (TransactionNotFound e) {
             throw new RuntimeException(e);
@@ -94,6 +178,18 @@ public class TaskService implements TaskDao {
     @Override
     public void delete(String id) {
 
+    }
+
+    public static Date stringToDate(String dateStr) {
+        try {
+            return DATE_FORMAT.parse(dateStr);
+        } catch (ParseException e) {
+            throw new RuntimeException("Invalid date format. Expected format: dd-MM-yyyy", e);
+        }
+    }
+
+    public static String dateToString(Date date) {
+        return DATE_FORMAT.format(date);
     }
 
 
