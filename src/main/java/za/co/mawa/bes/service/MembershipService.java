e84d836a5f5507b0a9262e9c842edcb0587dab5d
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2025-02-04 10:38:23 +0200

    ""

--- src/main/java/za/co/mawa/bes/service/MembershipService.java
+++ src/main/java/za/co/mawa/bes/service/MembershipService.java
@@ -55,6 +55,8 @@ public class MembershipService implements MembershipDao {
     FieldOptionService fieldOptionService;
     @Autowired
     InvoiceService invoiceService;
+    @Autowired
+    UserService userService;
 
     public MembershipDto create(MembershipCreateDto membershipCreateDto) throws PartnerNotFoundException, ProductNotFoundException, TransactionItemAddException, TransactionDateAddException, TransactionPartnerAddException {
 
@@ -97,10 +99,32 @@ public class MembershipService implements MembershipDao {
         if (Objects.equals(membershipCreateDto.getCreationType(), "TRANSFER")) {
             transactionCreateDto.setStatus(Status.PENDING);
             transactionCreateDto.setStatusReason(StatusReason.DOCUMENT_VERIFICATION);
-        } else {
+        }
+        else if (membershipCreateDto.getCreationType().equals("UPGRADE")){
+
+            transactionCreateDto.setStatus(Status.WAITING_PERIOD);
+        }
+        else {
             transactionCreateDto.setStatus(Status.NEW);
         }
         TransactionDto transactionDto = transactionService.create(transactionCreateDto);
+
+        if (membershipCreateDto.getCreationType().equals("UPGRADE")){
+            try {
+                TransactionLinkDto link = new TransactionLinkDto();
+                link.setTransaction1(transactionDto.getId());
+                link.setTransaction2(membershipCreateDto.getCurrentMembershipId());
+                link.setType(TransactionType.UPGRADE);
+                link.setCreateBy(userService.getCurrentUserPartnerId());
+                transactionService.addLink(link);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            transactionCreateDto.setStatus(Status.NEW);
+        }
         ProductDto productDto = productService.get(membershipCreateDto.getProductId());
         TransactionItemDto transactionItemDto = new TransactionItemDto();
         transactionItemDto.setTransaction(transactionDto.getId());
