c7f156f40cf212cf7ac509629d579b3b53d7283f
funi504 <nekhungunifunanani9@gmail.com>
2025-02-17 13:19:54 +0200

    MSBS-856 added Xero contact and item code field option

---
+++ src/main/resources/db/migration/all/V135__MSBS-856-xero-add-contact-id -and-item-code.sql
@@ -0,0 +1,5 @@
+INSERT INTO `field_option` (`code`, `field`, `description`, `valid_from`, `valid_to`)
+VALUES	('XER0-CONTACT-ID', 'ID-TYPE', 'Xero contact id', CURRENT_DATE, '9999-12-31');
+
+INSERT INTO `field_option` (`code`, `field`, `description`, `valid_from`, `valid_to`)
+VALUES	('XERO-ITEM-CODE', 'PRODUCT-ATTRIBUTE', 'Xero item code', CURRENT_DATE, '9999-12-31');
