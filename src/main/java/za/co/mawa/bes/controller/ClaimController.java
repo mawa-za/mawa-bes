488aacd44c89b26a0e86be0a3c1694f9ec5654cd
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2025-03-04 10:42:08 +0200

    '

--- src/main/java/za/co/mawa/bes/controller/ClaimController.java
+++ src/main/java/za/co/mawa/bes/controller/ClaimController.java
@@ -374,7 +374,7 @@ public class ClaimController {
                     }
                 }
 
-                String xeroInvoiceNumber = xeroAccountingService.createInvoice(claim.getMember().getId(), claim.getNumber(),itemCode);
+                String xeroInvoiceNumber = xeroAccountingService.createInvoice(getFuneralServiceProvider(), claim.getNumber(),itemCode);
                 paymentRequest.setReference(xeroInvoiceNumber);
                 paymentRequest.setDueDate(new Date());
                 paymentRequest.setRecipientId(claim.getClaimant().getId());
