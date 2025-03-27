991a2d1e495e822aa28f6e260844c14e35988d44
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2025-03-27 12:48:51 +0200

    ""

--- src/main/java/za/co/mawa/bes/service/MembershipService.java
+++ src/main/java/za/co/mawa/bes/service/MembershipService.java
@@ -635,13 +635,14 @@ public class MembershipService implements MembershipDao {
             dateEffective.setValue(date);
         }
         if (Objects.equals(membershipCreateDto.getCreationType(), "NEW") || Objects.equals(membershipCreateDto.getCreationType(), "UPGRADE")) {
-            if(membershipCreateDto.getDateJoined() != null){
-                dateEffective.setValue(addDaysToDate(membershipCreateDto.getDateJoined(), waitingPeriod));
+            if(membershipCreateDto.getDateJoined() !=  null){
+                if(waitingPeriod > 0){
+                    dateEffective.setValue(addDaysToDate(membershipCreateDto.getDateJoined(), waitingPeriod));
+                }
             }
             else {
                 dateEffective.setValue(addDaysToDate(date, waitingPeriod));
             }
-
         }
         transactionService.addDate(dateEffective);
     }
