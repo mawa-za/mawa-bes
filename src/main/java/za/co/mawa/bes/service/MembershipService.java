f2c86738f3aba3ebb372fb6db0590089eb6c646b
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2025-05-05 20:00:47 +0200

    ""

--- src/main/java/za/co/mawa/bes/service/MembershipService.java
+++ src/main/java/za/co/mawa/bes/service/MembershipService.java
@@ -118,7 +118,9 @@ public class MembershipService implements MembershipDao {
 
         if (membershipCreateDto.getCreationType().equalsIgnoreCase("UPGRADE")){
             try {
-                transactionCreateDto.setStatus(Status.UPGRADE_WAITING_PERIOD);
+                if(addDaysToDate(membershipCreateDto.getDateJoined(), getWaitingPeriod(membershipCreateDto.getProductId(), Status.UPGRADE_WAITING_PERIOD )).after(new Date())){
+                    transactionCreateDto.setStatus(Status.UPGRADE_WAITING_PERIOD);
+                }
                 TransactionItemDto latestItem = transactionService
                         .getItems(membershipCreateDto.getCurrentMembershipId())
                         .stream()
@@ -142,7 +144,9 @@ public class MembershipService implements MembershipDao {
                 itemEditDto.setTransaction(membershipCreateDto.getCurrentMembershipId());
                 itemEditDto.setItem(latestItem.getItem()); // Must specify which item to edit
                 itemEditDto.setProduct(latestItem.getProduct());
-                itemEditDto.setStatus(Status.INACTIVE);
+                if(!latestItem.getStatus().equalsIgnoreCase(Status.ACTIVE)){
+                    itemEditDto.setStatus(Status.INACTIVE);
+                }
                 itemEditDto.setValidTo(new Date()); // End the item's validity period now
                 transactionService.editItem(itemEditDto);
 
