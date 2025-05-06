db30969d0fd067b5b13bd1e8358c72d345834186
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2025-05-06 09:29:38 +0200

    ""

--- src/main/java/za/co/mawa/bes/utils/Status.java
+++ src/main/java/za/co/mawa/bes/utils/Status.java
@@ -26,9 +26,13 @@ public class Status {
     public static String AWAITING_APPROVAL = "AWAITING-APPROVAL";
 
 
+
+
     public static String WAITING_PERIOD = "WAITING-PERIOD";
     public static  String INSUFFICIENT_FUNDS = "INSUFFICIENT-FUNDS";
     public static String SERVICE_REQUEST_STATUS_REASON = "SERVICE-REQUEST-STATUS-REASON";
+    public static String UPGRADE_WAITING_PERIOD = "UPGRADE-WAITING-PERIOD";
+
 
 
 }
