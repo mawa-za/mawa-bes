b8eba57b984432d47d3e0db900bf96f1c928b904
MthetheleliPK <103575295+MthetheleliPK@users.noreply.github.com>
2025-05-14 08:49:45 +0200

    ""

--- src/main/java/za/co/mawa/bes/service/MembershipService.java
+++ src/main/java/za/co/mawa/bes/service/MembershipService.java
@@ -659,10 +659,13 @@ public class MembershipService implements MembershipDao {
             Set<TransactionViewEntity> uniqueEntities = new HashSet<>(entities);
             MembershipEditDto editDto = new MembershipEditDto();
 
+            TransactionDto transactionDto = new TransactionDto();
+
             for (TransactionViewEntity entity : uniqueEntities) {
                 if(entity.getTransactionStatus().equalsIgnoreCase(Status.AWAITING_APPROVAL) || entity.getTransactionStatus().equalsIgnoreCase(Status.INACTIVE)){
                     continue;
                 }
+                transactionDto.setId(entity.getTransactionId());
                 //fetching the waiting period of the membership product
                 int waitingPeriod = 0;
 
@@ -732,6 +735,7 @@ public class MembershipService implements MembershipDao {
                         }
                     }
                 }
+                enforceProductStatusRules(transactionDto);
             }
             return "Validated";
 
