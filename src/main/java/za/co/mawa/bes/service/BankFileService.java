f3cbf9e71c2b5745578106955782504b58d15a5b
tebogo.mohale <tebogo.mohale@raretag.co.za>
2025-05-19 21:29:38 +0200

    MSBS-1205 Move Group Header to the top of the XML file

--- src/main/java/za/co/mawa/bes/service/BankFileService.java
+++ src/main/java/za/co/mawa/bes/service/BankFileService.java
@@ -108,7 +108,7 @@ public class BankFileService {
             bankFileXmlDto.setAmount(totalAmount);
             bankFileXmlDto.setNumberOfTransactions(paymentRequestDtoList.size());
             Element grpHdr = groupHeader(bankFileXmlDto, doc);
-            cstmrCdtTrfInitn.appendChild(grpHdr);
+            cstmrCdtTrfInitn.insertBefore(grpHdr,cstmrCdtTrfInitn.getFirstChild());
 
 
             documentElement.appendChild(cstmrCdtTrfInitn);
