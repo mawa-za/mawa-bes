e42247d1a79fe58153128a7d7ff7f9f62e62518a
TebogoMohale <tebogo.mohale@mawa.co.za>
2026-04-30 17:48:02 +0200

    Introduce enhanced membership management endpoints in MembershipController and MembershipControllerV2

--- src/main/java/za/co/mawa/bes/controller/MembershipController.java
+++ src/main/java/za/co/mawa/bes/controller/MembershipController.java
@@ -449,4 +449,48 @@ public class MembershipController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         }
     }
+
+
+    @RequestMapping(value = "mawa-pay", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getMemberships(@RequestParam(required = false) String status,
+                                            @RequestParam(required = false) String mainPartner,
+                                            @RequestParam(required = false) String employeeResponsibleName,
+                                            @RequestParam(required = false) String creationDate,
+                                            @RequestParam(required = false) String idNumber) {
+        try {
+            TransactionViewDto transactionViewDto = new TransactionViewDto();
+            transactionViewDto.setType(TransactionType.MEMBERSHIP);
+
+            if(status != null && status != "") {
+                transactionViewDto.setStatus(status);
+            }
+
+            if(employeeResponsibleName != null && employeeResponsibleName != "") {
+                transactionViewDto.setEmployeeResponsibleName(employeeResponsibleName);
+            }
+
+            if(mainPartner != null && mainPartner != "") {
+                transactionViewDto.setMainPartner(mainPartner);
+            }
+
+            if(idNumber != null && idNumber != "") {
+                transactionViewDto.setIdNumber(idNumber);
+            }
+
+            if (creationDate != null) {
+
+                // Define the formatter for the input string
+                SimpleDateFormat formatter = new SimpleDateFormat("MMM dd, yyyy");
+
+                // Parse the string into Date object
+                Date date = formatter.parse(creationDate);
+
+                transactionViewDto.setCreationDate(date);
+            }
+
+            return ResponseEntity.ok(gson.toJson(transactionService.searchV2(transactionViewDto)));
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception);
+        }
+    }
 }
