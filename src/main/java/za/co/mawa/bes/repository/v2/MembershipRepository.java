0af048767331585a7ba2c98e68254ef2e6705d22
TebogoMohale <tebogo.mohale@mawa.co.za>
2026-05-09 15:08:12 +0200

    Support filtering memberships by multiple memberIds with `JpaSpecificationExecutor`.

--- src/main/java/za/co/mawa/bes/repository/v2/MembershipRepository.java
+++ src/main/java/za/co/mawa/bes/repository/v2/MembershipRepository.java
@@ -3,11 +3,13 @@ package za.co.mawa.bes.repository.v2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import za.co.mawa.bes.entity.v2.MembershipEntity;
 
 import java.util.Optional;
 
-public interface MembershipRepository extends JpaRepository<MembershipEntity, String> {
+public interface MembershipRepository extends JpaRepository<MembershipEntity, String> , JpaSpecificationExecutor<MembershipEntity>
+{
 
     Optional<MembershipEntity> findByOldId(String oldId);
     Page<MembershipEntity> findByMemberId(String memberId, Pageable pageable);
