bc3229182bfa1c5ffade4c28eab376e57e8667d8
tebogo.mohale <tebogo.mohale@mawa.co.za>
2026-08-01 17:08:15 +0200

    feat: Add setter methods for all fields in GroupSocietyClaimDebitRequest

--- src/main/java/za/co/mawa/bes/dto/v2/group/GroupSocietyClaimDebitRequest.java
+++ src/main/java/za/co/mawa/bes/dto/v2/group/GroupSocietyClaimDebitRequest.java
@@ -14,19 +14,39 @@ public class GroupSocietyClaimDebitRequest {
         return amountCents;
     }
 
+    public void setAmountCents(Long amountCents) {
+        this.amountCents = amountCents;
+    }
+
     public LocalDate getClaimDate() {
         return claimDate;
+    }
+
+    public void setClaimDate(LocalDate claimDate) {
+        this.claimDate = claimDate;
     }
 
     public String getClaimId() {
         return claimId;
     }
 
+    public void setClaimId(String claimId) {
+        this.claimId = claimId;
+    }
+
     public String getClaimNo() {
         return claimNo;
     }
 
+    public void setClaimNo(String claimNo) {
+        this.claimNo = claimNo;
+    }
+
     public String getNotes() {
         return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
     }
 }
