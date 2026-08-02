664effa2bb141fad9e5871df67a88361926b4733
tebogo.mohale <tebogo.mohale@mawa.co.za>
2026-08-02 14:27:57 +0200

    refactor: change handlers type from List to ObjectProvider in ApprovalSubmissionHandlerRegistry for improved handler management

--- src/main/java/za/co/mawa/bes/service/v2/ApprovalSubmissionHandlerRegistry.java
+++ src/main/java/za/co/mawa/bes/service/v2/ApprovalSubmissionHandlerRegistry.java
@@ -1,19 +1,18 @@
 package za.co.mawa.bes.service.v2;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Component;
 import za.co.mawa.bes.entity.v2.ApprovalRequestEntity;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class ApprovalSubmissionHandlerRegistry {
 
-    private final List<ApprovalSubmissionHandler> handlers;
+    private final ObjectProvider<ApprovalSubmissionHandler> handlers;
 
     public void handleSubmit(ApprovalRequestEntity approvalRequest, String actionBy) {
-        handlers.stream()
+        handlers.orderedStream()
                 .filter(handler -> handler.supports() == approvalRequest.getApprovalType())
                 .findFirst()
                 .ifPresent(handler -> handler.onSubmit(approvalRequest, actionBy));
